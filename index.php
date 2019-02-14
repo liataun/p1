a10a78a62e570763f1cf555b6601085b80123d0a
Brian Twitchell <32440651+liataun@users.noreply.github.com>
2019-02-13 21:48:42 -0500

    Content edits to html and css. README edits.

--- index.php
+++ index.php
@@ -10,14 +10,15 @@
 </head>
 <body>
 	<div>
-		<h1 class="content" id="name">Brian</h1>
-		<p class="content">I work heavily with databases and other oft-repeated tasks in support of production ETL websites, so I am hoping to expand my application programming skills, AI knowledge, and Web/Cloud experience.</p>
-		<p class="content">I am taking this class as a blend of building my confidence, developing skills for work, and hopefully getting a Master's degree, though I haven't decided for sure if the HES Software Engineering focus is the right fit. I have a habit of long posts and over-thinking, but I love to share what I know, so feel free to ask me questions if any of my posts interest you!</p>
 		<img src='/images/photo.jpg' alt='At Middlesex Fells'>
-		<p class="content" id="quote"><?=$quote?></p>
+		<h1 id="name">Brian</h1>
 	</div>
-	<div class="extra">
-		<?php dump($quote); ?>
+	<div class="content">
+		<h3>I work heavily with databases and other oft-repeated tasks in support of production ETL websites, so I am hoping to expand my application programming skills, AI knowledge, and Web/Cloud experience.</h3>
+		<h3>I am taking this class as a blend of building my confidence, developing skills for work, and starting a Master's degree, though I haven't decided for sure if the HES Software Engineering focus is the right fit. I have a habit of long posts and over-thinking, but I love to share what I know, so feel free to ask me questions if any of my posts interest you!</h3>
+		<h3>Apologies for the terrile Graphic Design of this site page. It has been a while since I was last able to play with the full content of a page, so I will be fiddling with a variety of things to remind myself what works and what does not.</h3>
 	</div>
+	<p><span id="quote"><?=$quote?></span></p>
+	<p>&mdash;<span id="author"><?=$author?></span></p>
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,4 +1,16 @@
 <?php
-$array = ["one","two","three","four"];
+$arrayText = [
+	"Weapons are not auspicious tools/some things are simply bad/thus the Taoist shuns them",
+	"To erase the possibility of empathy is to erase the possibility of undersanding.",
+	"The 'grammar bullies' - you read them in places like the New York Times and they tell you what is correct...This is Judgementalism. The game that is being played there is a game of social class.",
+	"Surfeit! When the Daffodil/Doth of the Dew-/Even as Herself-Sir-/I will-of You-",
+];
+$arrayAuthor = [
+	"TaoTeChing as translated by Red Pine",
+	"Amanda Palmer in The Art of Asking p.289",
+	"Ursula K. Le Guin in Conversations on Writing p.24",
+	"Emily Dickenson",
+];
 $number = rand(0,3);
-$quote = $array[$number];
+$quote = $arrayText[$number];
+$author = $arrayAuthor[$number];
